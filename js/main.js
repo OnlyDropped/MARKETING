a0528304a6a94201d35257cec7c1f36904cd9927
OnlyDropped <asheburenkov@yandex.ru>
2021-02-26 17:26:24 +0300

    add responsive

--- js/main.js
+++ js/main.js
@@ -5,8 +5,19 @@ $(function(){
   $('.slider-blog__inner').slick({
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.svg" alt=""></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.svg" alt=""></button>'
-  });
+    nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.svg" alt=""></button>',
+      responsive: [
+        {
+          breakpoint: 769,
+          settings: {
+            arrows: false
+          }
+        },
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
   $('.menu__btn').on('click', function(){
     $('.header__top-inner').toggleClass('header__top-inner--active');
   });
